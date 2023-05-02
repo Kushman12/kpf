35e5245c096ae0bec2c7c2d85c705acd96bb902c
hilda1737 <hildamoraahh@gmail.com>
2023-05-02 14:57:28 +0300

    home page

---
+++ kpf-app/home.js
@@ -0,0 +1,848 @@
+import React, { Component } from "react";
+import styled, { css } from "styled-components";
+import IoniconsIcon from "react-native-vector-icons/dist/Ionicons";
+import MaterialCommunityIconsIcon from "react-native-vector-icons/dist/MaterialCommunityIcons";
+import SimpleLineIconsIcon from "react-native-vector-icons/dist/SimpleLineIcons";
+import FeatherIcon from "react-native-vector-icons/dist/Feather";
+import EntypoIcon from "react-native-vector-icons/dist/Entypo";
+import EvilIconsIcon from "react-native-vector-icons/dist/EvilIcons";
+import CupertinoButtonInfo3 from "../components/CupertinoButtonInfo3";
+import CupertinoButtonInfo from "../components/CupertinoButtonInfo";
+import CupertinoButtonInfo1 from "../components/CupertinoButtonInfo1";
+import CupertinoButtonInfo2 from "../components/CupertinoButtonInfo2";
+import MaterialButtonPrimary from "../components/MaterialButtonPrimary";
+
+function Kpf(props) {
+  return (
+    <Container>
+      <Rect6>
+        <Icon12ColumnRow>
+          <Icon12Column>
+            <IoniconsIcon
+              name="ios-people"
+              style={{
+                color: "rgba(255,255,255,1)",
+                fontSize: 40,
+                height: 44,
+                width: 35,
+                marginLeft: 32
+              }}
+            ></IoniconsIcon>
+            <LoremIpsum21>1234</LoremIpsum21>
+          </Icon12Column>
+          <LoremIpsum20></LoremIpsum20>
+          <Icon14Column>
+            <IoniconsIcon
+              name="ios-checkmark-circle"
+              style={{
+                color: "rgba(255,255,255,1)",
+                fontSize: 40,
+                height: 44,
+                width: 33,
+                marginLeft: 36
+              }}
+            ></IoniconsIcon>
+            <LoremIpsum22>1234</LoremIpsum22>
+          </Icon14Column>
+          <Text3>011A41</Text3>
+          <Icon13Stack>
+            <MaterialCommunityIconsIcon
+              name="account-multiple-plus"
+              style={{
+                top: 0,
+                left: 41,
+                position: "absolute",
+                color: "rgba(255,255,255,1)",
+                fontSize: 40,
+                height: 44,
+                width: 40
+              }}
+            ></MaterialCommunityIconsIcon>
+            <LoremIpsum24>1234</LoremIpsum24>
+          </Icon13Stack>
+          <SimpleLineIconsIcon
+            name="badge"
+            style={{
+              color: "rgba(255,255,255,1)",
+              fontSize: 40,
+              height: 45,
+              width: 40,
+              marginLeft: 224,
+              marginTop: 6
+            }}
+          ></SimpleLineIconsIcon>
+        </Icon12ColumnRow>
+        <HappyClientsRow>
+          <HappyClients>Happy Clients</HappyClients>
+          <LoremIpsum23>Students we have taught</LoremIpsum23>
+          <DedicatedStaff>Dedicated Staff</DedicatedStaff>
+          <Achievements>Achievements</Achievements>
+        </HappyClientsRow>
+      </Rect6>
+      <Rect4>
+        <Group5Row>
+          <Group5>
+            <FeatherIcon
+              name="x-circle"
+              style={{
+                color: "rgba(53,94,252,1)",
+                fontSize: 40
+              }}
+            ></FeatherIcon>
+            <NoHiddenCost>No Hidden Cost</NoHiddenCost>
+            <LoremIpsum15>Our cost are clearly listed</LoremIpsum15>
+          </Group5>
+          <Group4>
+            <LoremIpsum11Stack>
+              <LoremIpsum11></LoremIpsum11>
+              <Icon9Stack>
+                <IoniconsIcon
+                  name="md-people"
+                  style={{
+                    top: 0,
+                    left: 0,
+                    position: "absolute",
+                    color: "rgba(53,94,252,1)",
+                    fontSize: 40
+                  }}
+                ></IoniconsIcon>
+                <DedicatedTeam>Dedicated Team</DedicatedTeam>
+              </Icon9Stack>
+            </LoremIpsum11Stack>
+            <LoremIpsum16>
+              Our company is dedicated to {"\n"}helping you grow
+            </LoremIpsum16>
+          </Group4>
+          <Group3>
+            <Icon10Row>
+              <IoniconsIcon
+                name="md-call"
+                style={{
+                  color: "rgba(53,94,252,1)",
+                  fontSize: 40
+                }}
+              ></IoniconsIcon>
+              <LoremIpsum19>24/7 Available</LoremIpsum19>
+            </Icon10Row>
+            <LoremIpsum18>We are just one call away</LoremIpsum18>
+          </Group3>
+        </Group5Row>
+      </Rect4>
+      <LoremIpsum>Home About Services Pages Contact</LoremIpsum>
+      <LoremIpsum2Stack>
+        <LoremIpsum2>info@example.com +254 7345 6789</LoremIpsum2>
+        <IoniconsIcon
+          name="ios-call"
+          style={{
+            top: 0,
+            left: 136,
+            position: "absolute",
+            color: "rgba(128,128,128,1)",
+            fontSize: 40
+          }}
+        ></IoniconsIcon>
+      </LoremIpsum2Stack>
+      <EntypoIcon
+        name="facebook-with-circle"
+        style={{
+          top: -75,
+          left: 819,
+          position: "absolute",
+          color: "rgba(128,128,128,1)",
+          fontSize: 40
+        }}
+      ></EntypoIcon>
+      <Rect></Rect>
+      <EntypoIcon
+        name="linkedin-with-circle"
+        style={{
+          top: 77,
+          left: 1294,
+          position: "absolute",
+          color: "rgba(128,128,128,1)",
+          fontSize: 40
+        }}
+      ></EntypoIcon>
+      <EntypoIcon
+        name="tumblr-with-circle"
+        style={{
+          top: 77,
+          left: 1167,
+          position: "absolute",
+          color: "rgba(53,94,252,1)",
+          fontSize: 40
+        }}
+      ></EntypoIcon>
+      <EntypoIcon
+        name="facebook-with-circle"
+        style={{
+          top: 77,
+          left: 1226,
+          position: "absolute",
+          color: "rgba(53,94,252,1)",
+          fontSize: 40
+        }}
+      ></EntypoIcon>
+      <LoremIpsum3Stack>
+        <LoremIpsum3>123 Street, Nakuru, Kenya 9.00 am - 9.00 pm</LoremIpsum3>
+        <EvilIconsIcon
+          name="clock"
+          style={{
+            top: 0,
+            left: 167,
+            position: "absolute",
+            color: "rgba(128,128,128,1)",
+            fontSize: 40
+          }}
+        ></EvilIconsIcon>
+      </LoremIpsum3Stack>
+      <EntypoIcon
+        name="location-pin"
+        style={{
+          top: 9,
+          left: 39,
+          position: "absolute",
+          color: "rgba(128,128,128,1)",
+          fontSize: 40
+        }}
+      ></EntypoIcon>
+      <Kpforex>KPFOREX</Kpforex>
+      <Rect2></Rect2>
+      <Text>
+        Welcome to KPForex{"\n"}Your Financial Status Is Our Goal{"\n"}explore
+        more
+      </Text>
+      <Text2></Text2>
+      <LoremIpsum7>We Help Our Students {"\n"} Grow Their Money</LoremIpsum7>
+      <LoremIpsum10></LoremIpsum10>
+      <LoremIpsum12>
+        Tempor erat elitr rebum at clita.{"\n"} Diam dolor diam ipsum sit.{" "}
+        {"\n"}Aliqu diam amet diam et eos.{"\n"} Clita erat ipsum et lorem et
+        sit, {"\n"}sed stet lorem sit clita duo justo{"\n"}magna dolore erat
+        amet
+      </LoremIpsum12>
+      <Rect5></Rect5>
+      <LoremIpsum14></LoremIpsum14>
+      <LoremIpsum26Stack>
+        <LoremIpsum26></LoremIpsum26>
+        <Rect8>
+          <IoniconsIcon
+            name="md-checkmark"
+            style={{
+              color: "rgba(24,16,234,1)",
+              fontSize: 80,
+              height: 87,
+              width: 60,
+              marginLeft: 29
+            }}
+          ></IoniconsIcon>
+        </Rect8>
+      </LoremIpsum26Stack>
+      <LoremIpsum27>Few Reasons Why You{"\n"} should Choose Us!</LoremIpsum27>
+      <LoremIpsum28>
+        Tempor erat elitr rebum at clita. Diam dolo{"\n"}r diam ipsum sit.{"\n"}{" "}
+        Aliqu diam amet diam et eos. Clita e{"\n"}rat ipsum et lorem et {"\n"}
+        sit, sed stet lorem sit clita duo justo{"\n"} magna dolore erat amet
+      </LoremIpsum28>
+      <CupertinoButtonInfo3
+        style={{
+          height: 44,
+          width: 100,
+          position: "absolute",
+          left: 72,
+          top: 346
+        }}
+      ></CupertinoButtonInfo3>
+      <CupertinoButtonInfo
+        style={{
+          height: 44,
+          width: 141,
+          position: "absolute",
+          left: 145,
+          top: 1722
+        }}
+      ></CupertinoButtonInfo>
+      <CupertinoButtonInfo1
+        style={{
+          height: 44,
+          width: 100,
+          position: "absolute",
+          left: 59,
+          top: 1402,
+          overflow: "scroll"
+        }}
+      ></CupertinoButtonInfo1>
+      <CupertinoButtonInfo2
+        style={{
+          height: 44,
+          width: 100,
+          position: "absolute",
+          left: 776,
+          top: 456
+        }}
+      ></CupertinoButtonInfo2>
+      <EntypoIcon
+        name="linkedin-with-circle"
+        style={{
+          top: 77,
+          left: 1118,
+          position: "absolute",
+          color: "rgba(53,94,252,1)",
+          fontSize: 40
+        }}
+      ></EntypoIcon>
+      <Rect9>
+        <IoniconsIcon
+          name="md-checkmark"
+          style={{
+            color: "rgba(24,16,234,1)",
+            fontSize: 80,
+            height: 87,
+            width: 60,
+            marginTop: 2,
+            marginLeft: 7
+          }}
+        ></IoniconsIcon>
+      </Rect9>
+      <Rect10>
+        <IoniconsIcon
+          name="md-checkmark"
+          style={{
+            color: "rgba(24,16,234,1)",
+            fontSize: 80,
+            height: 87,
+            width: 60,
+            marginTop: 12,
+            marginLeft: 4
+          }}
+        ></IoniconsIcon>
+      </Rect10>
+      <MaterialButtonPrimary
+        style={{
+          height: 36,
+          width: 100,
+          position: "absolute",
+          left: 551,
+          top: 1952
+        }}
+      ></MaterialButtonPrimary>
+      <LoremIpsum29>Awesome Forex Classes{"\n"}in Kenya</LoremIpsum29>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  display: flex;
+  background-color: rgba(255,255,255,0.76);
+  flex-direction: column;
+  position: relative;
+  height: 100vh;
+  width: 100vw;
+`;
+
+const Rect6 = styled.div`
+  top: 1157px;
+  left: 8px;
+  width: 1270px;
+  height: 191px;
+  position: absolute;
+  background-color: rgba(53,94,252,1);
+  flex-direction: column;
+  display: flex;
+`;
+
+const LoremIpsum21 = styled.span`
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 400;
+  color: rgba(255,255,255,1);
+  font-size: 44px;
+`;
+
+const Icon12Column = styled.div`
+  width: 99px;
+  flex-direction: column;
+  display: flex;
+  margin-top: 3px;
+  margin-bottom: 2px;
+`;
+
+const LoremIpsum20 = styled.span`
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 400;
+  color: #121212;
+  margin-left: 99px;
+  margin-top: 6px;
+`;
+
+const LoremIpsum22 = styled.span`
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 400;
+  color: rgba(255,255,255,1);
+  font-size: 44px;
+  margin-top: 5px;
+`;
+
+const Icon14Column = styled.div`
+  width: 99px;
+  flex-direction: column;
+  display: flex;
+  margin-left: 126px;
+`;
+
+const Text3 = styled.span`
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 400;
+  color: #121212;
+  margin-left: 152px;
+  margin-top: 76px;
+`;
+
+const LoremIpsum24 = styled.span`
+  font-family: Roboto;
+  top: 42px;
+  left: 0px;
+  position: absolute;
+  font-style: normal;
+  font-weight: 400;
+  color: rgba(255,255,255,1);
+  font-size: 44px;
+`;
+
+const Icon13Stack = styled.div`
+  width: 99px;
+  height: 94px;
+  margin-left: 69px;
+  margin-top: 7px;
+  position: relative;
+`;
+
+const Icon12ColumnRow = styled.div`
+  height: 101px;
+  flex-direction: row;
+  display: flex;
+  margin-top: 1px;
+  margin-left: 60px;
+  margin-right: 155px;
+`;
+
+const HappyClients = styled.span`
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 400;
+  color: rgba(255,255,255,1);
+  font-size: 22px;
+  margin-top: 4px;
+`;
+
+const LoremIpsum23 = styled.span`
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 400;
+  color: rgba(255,255,255,1);
+  font-size: 19px;
+  margin-left: 166px;
+  margin-top: 1px;
+`;
+
+const DedicatedStaff = styled.span`
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 400;
+  color: rgba(255,255,255,1);
+  font-size: 22px;
+  margin-left: 182px;
+  margin-top: 4px;
+`;
+
+const Achievements = styled.span`
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 400;
+  color: rgba(255,255,255,1);
+  font-size: 21px;
+  margin-left: 172px;
+`;
+
+const HappyClientsRow = styled.div`
+  height: 29px;
+  flex-direction: row;
+  display: flex;
+  margin-top: 7px;
+  margin-left: 44px;
+  margin-right: 79px;
+`;
+
+const Rect4 = styled.div`
+  top: 973px;
+  left: 52px;
+  width: 1156px;
+  height: 163px;
+  position: absolute;
+  background-color: rgba(255,255,255,0.3864);
+  border-radius: 27px;
+  border-width: 2px;
+  border-color: rgba(0,0,0,0.92);
+  border-style: solid;
+  flex-direction: row;
+  display: flex;
+`;
+
+const Group5 = styled.div`
+  width: 192px;
+  height: 99px;
+  flex-direction: column;
+  display: flex;
+  margin-top: 8px;
+`;
+
+const NoHiddenCost = styled.span`
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 400;
+  color: #121212;
+  font-size: 22px;
+  margin-left: 38px;
+`;
+
+const LoremIpsum15 = styled.span`
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 400;
+  color: #121212;
+  font-size: 13px;
+  margin-top: 18px;
+  margin-left: 17px;
+`;
+
+const Group4 = styled.div`
+  width: 215px;
+  height: 119px;
+  flex-direction: column;
+  display: flex;
+  margin-left: 206px;
+`;
+
+const LoremIpsum11 = styled.span`
+  font-family: Roboto;
+  top: 31px;
+  left: 35px;
+  position: absolute;
+  font-style: normal;
+  font-weight: 400;
+  color: #121212;
+`;
+
+const DedicatedTeam = styled.span`
+  font-family: Roboto;
+  top: 43px;
+  left: 30px;
+  position: absolute;
+  font-style: normal;
+  font-weight: 400;
+  color: #121212;
+  font-size: 23px;
+`;
+
+const Icon9Stack = styled.div`
+  top: 0px;
+  left: 0px;
+  width: 196px;
+  height: 70px;
+  position: absolute;
+`;
+
+const LoremIpsum11Stack = styled.div`
+  width: 196px;
+  height: 70px;
+  position: relative;
+`;
+
+const LoremIpsum16 = styled.span`
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 400;
+  color: #121212;
+  font-size: 16px;
+  margin-top: 11px;
+  margin-left: 8px;
+`;
+
+const Group3 = styled.div`
+  width: 224px;
+  height: 87px;
+  flex-direction: column;
+  display: flex;
+  margin-left: 223px;
+  margin-top: 18px;
+`;
+
+const LoremIpsum19 = styled.span`
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 400;
+  color: #121212;
+  font-size: 19px;
+  margin-left: 48px;
+  margin-top: 19px;
+`;
+
+const Icon10Row = styled.div`
+  height: 44px;
+  flex-direction: row;
+  display: flex;
+  margin-right: 25px;
+`;
+
+const LoremIpsum18 = styled.span`
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 400;
+  color: #121212;
+  font-size: 20px;
+  margin-top: 19px;
+`;
+
+const Group5Row = styled.div`
+  height: 119px;
+  flex-direction: row;
+  display: flex;
+  flex: 1 1 0%;
+  margin-right: 70px;
+  margin-left: 27px;
+  margin-top: 26px;
+`;
+
+const LoremIpsum = styled.span`
+  font-family: Roboto;
+  top: 87px;
+  left: 766px;
+  position: absolute;
+  font-style: normal;
+  font-weight: 700;
+  color: rgba(53,94,252,1);
+  font-size: 20px;
+  opacity: 0.98;
+`;
+
+const LoremIpsum2 = styled.span`
+  font-family: Roboto;
+  top: 15px;
+  left: 0px;
+  position: absolute;
+  font-style: normal;
+  font-weight: 400;
+  color: #121212;
+`;
+
+const LoremIpsum2Stack = styled.div`
+  top: 0px;
+  left: 997px;
+  width: 267px;
+  height: 44px;
+  position: absolute;
+`;
+
+const Rect = styled.div`
+`;
+
+const LoremIpsum3 = styled.span`
+  font-family: Roboto;
+  top: 13px;
+  left: 0px;
+  position: absolute;
+  font-style: normal;
+  font-weight: 400;
+  color: #121212;
+  font-size: 15px;
+`;
+
+const LoremIpsum3Stack = styled.div`
+  top: 9px;
+  left: 79px;
+  width: 324px;
+  height: 44px;
+  position: absolute;
+`;
+
+const Kpforex = styled.span`
+  font-family: Roboto;
+  top: 83px;
+  left: 151px;
+  position: absolute;
+  font-style: normal;
+  font-weight: 400;
+  color: #121212;
+`;
+
+const Rect2 = styled.div`
+`;
+
+const Text = styled.span`
+  font-family: Roboto;
+  top: 205px;
+  left: 39px;
+  position: absolute;
+  font-style: normal;
+  font-weight: 400;
+  color: #121212;
+  font-size: 36px;
+`;
+
+const Text2 = styled.span`
+  font-family: Roboto;
+  top: 522px;
+  left: 403px;
+  position: absolute;
+  font-style: normal;
+  font-weight: 400;
+  color: #121212;
+  font-size: 13px;
+`;
+
+const LoremIpsum7 = styled.span`
+  font-family: Roboto;
+  top: 538px;
+  left: 729px;
+  position: absolute;
+  font-style: normal;
+  font-weight: 400;
+  color: #121212;
+  font-size: 41px;
+`;
+
+const LoremIpsum10 = styled.span`
+  font-family: Roboto;
+  top: 933px;
+  left: 334px;
+  position: absolute;
+  font-style: normal;
+  font-weight: 400;
+  color: #121212;
+  font-size: 80px;
+`;
+
+const LoremIpsum12 = styled.span`
+  font-family: Roboto;
+  top: 671px;
+  left: 729px;
+  position: absolute;
+  font-style: normal;
+  font-weight: 400;
+  color: #121212;
+  font-size: 25px;
+  opacity: 0.94;
+`;
+
+const Rect5 = styled.div`
+`;
+
+const LoremIpsum14 = styled.span`
+  font-family: Roboto;
+  top: 846px;
+  left: 204px;
+  position: absolute;
+  font-style: normal;
+  font-weight: 400;
+  color: #121212;
+`;
+
+const LoremIpsum26 = styled.span`
+  font-family: Roboto;
+  top: 202px;
+  left: 180px;
+  position: absolute;
+  font-style: normal;
+  font-weight: 400;
+  color: #121212;
+`;
+
+const Rect8 = styled.div`
+  top: 0px;
+  left: 0px;
+  width: 316px;
+  height: 250px;
+  position: absolute;
+  background-color: rgba(255,255,255,1);
+  border-radius: 24px;
+  border-width: 1px;
+  border-color: #000000;
+  flex-direction: column;
+  display: flex;
+  border-style: solid;
+`;
+
+const LoremIpsum26Stack = styled.div`
+  top: 1378px;
+  left: 630px;
+  width: 316px;
+  height: 250px;
+  position: absolute;
+`;
+
+const LoremIpsum27 = styled.span`
+  font-family: Roboto;
+  top: 1458px;
+  left: 50px;
+  position: absolute;
+  font-style: normal;
+  font-weight: 400;
+  color: #121212;
+  font-size: 35px;
+`;
+
+const LoremIpsum28 = styled.span`
+  font-family: Roboto;
+  top: 1566px;
+  left: 44px;
+  position: absolute;
+  font-style: normal;
+  font-weight: 400;
+  color: #121212;
+`;
+
+const Rect9 = styled.div`
+  top: 1458px;
+  left: 951px;
+  width: 316px;
+  height: 250px;
+  position: absolute;
+  background-color: rgba(255,255,255,1);
+  border-radius: 24px;
+  border-width: 1px;
+  border-color: #000000;
+  flex-direction: column;
+  display: flex;
+  border-style: solid;
+`;
+
+const Rect10 = styled.div`
+  top: 1635px;
+  left: 601px;
+  width: 316px;
+  height: 250px;
+  position: absolute;
+  background-color: rgba(255,255,255,1);
+  border-radius: 24px;
+  border-width: 1px;
+  border-color: #000000;
+  flex-direction: column;
+  display: flex;
+  border-style: solid;
+`;
+
+const LoremIpsum29 = styled.span`
+  font-family: Roboto;
+  top: 2025px;
+  left: 328px;
+  position: absolute;
+  font-style: normal;
+  font-weight: 400;
+  color: rgba(1,26,65,1);
+  font-size: 37px;
+`;
+
+export default Kpf;
